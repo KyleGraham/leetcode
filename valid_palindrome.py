e2e92ed9c9599d66f15cc92e9dcb62449ebc9bf7
Graham, Kyle <kyle.graham@sap.com>
2025-03-26 16:11:28 -0700

    Adding solution with better space complexity

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,6 +1,6 @@
 #solution using two pointers
 #time complexity: O(n)
-#space complexity: O(1)
+#space complexity: O(n)
 #remove any non alphanumeric characters and convert to lowercase
 # loop through the string starting from the front and back and check that the characters are the same
 # if they are not, return False
@@ -22,4 +22,26 @@ class Solution:
             j = len(newStr) - 1 - i
             if newStr[i] != newStr[j]:
                 return False
+        return True
+
+#better space complexity solution solution using two pointers
+#time complexity: O(n)
+#space complexity: O(1)
+#increments/decrements pointers if a non alphanumeric character is found
+#converts characters to lowercase and compares them
+#returns false if characters are not the same
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while j > i and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
         return True
