310ecfb0ab55c55b55ac3f0cfe09cbb77981448b
Kyle Graham <17037205+KyleGraham@users.noreply.github.com>
2025-03-26 19:05:49 -0700

    Problem 13

---
+++ container_with_most_water.py
@@ -0,0 +1,20 @@
+# optimal solution using two pointers
+# time complexity O(n)
+# space complexity O(1)
+# usual two pointer setup
+# calculate area based on distance between right and left pointer, and the minimun height
+# instead of incrementing both pointers, only increment the one that's the smaller height
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        l = 0
+        r = len(heights) - 1
+        while l < r:
+            area = (r - l) * min(heights[r], heights[l])
+            if area > maxArea:
+                maxArea = area
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxArea
