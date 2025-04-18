feee55ed6250b10401af546cb24a3fd0c6791a3c
Graham, Kyle <17037205+KyleGraham@users.noreply.github.com>
2025-04-18 12:32:57 -0700

    adding rotted fruit leetcode solution since better

--- 11_graphs/5_rotting_fruit.py
+++ 11_graphs/5_rotting_fruit.py
@@ -15,6 +15,90 @@
 # Input: grid = [[1,0,1],[0,2,0],[1,0,1]]
 # Output: -1
 
+#leetcode solution
+#time complexity O(m * n)
+#space complexity O(m * n)
+
+#the queue we use here is of rotten oranges instead
+#and we keep track of the fresh in a counter
+#we use a queue for the fruit that are rotting because if we get them to rotted right away, then the next value may rot earlier than expected
+
+#initialize the rows and cols of the grid
+#initialize a deque
+
+#fill the rotten fruit deque and the fresh fruit counter
+#loop through rows and cols
+#if the val is a 2, add to queue
+#otherwise, if the val is a 1, increment the fresh counter
+#append a round / level ticker to the queue as (-1, -1)
+#initialize minutes to -1
+#initialize the directions array
+#while the queue is not empty
+#pop the left item from the queue to get row and col
+#check if the row and col are -1, meaning we finished one round of processing
+#increment the minutes
+#check if the queue is not empty, to avoid the endless loop
+#append (-1, -1) to the queue if so
+
+#otherwise, this is a rotten orange
+#then it would contaminate its neighbors
+#loop through the directions array
+#initialize new row and col variables taking the row/col from the queue and adding the directions row/col
+#check that the new row and col are in range of the grid and if the item is a 1 meaning fresh fruit
+#if so, it's found a rotten fruit near a fresh fruit thats in bounds
+#decrement the fresh counter
+#set the item in the grid to 2 meaning rotten fruit
+#add item to the rotten fruit queue
+#return the minutes if no fresh fruit left, else -1
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = deque()
+
+        # Step 1). build the initial set of rotten oranges
+        fresh_oranges = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh_oranges += 1
+
+        # Mark the round / level, _i.e_ the ticker of timestamp
+        queue.append((-1, -1))
+
+        # Step 2). start the rotting process via BFS
+        minutes_elapsed = -1
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        while queue:
+            row, col = queue.popleft()
+            if row == -1:
+                # We finish one round of processing
+                minutes_elapsed += 1
+                if queue:  # to avoid the endless loop
+                    queue.append((-1, -1))
+            else:
+                # this is a rotten orange
+                # then it would contaminate its neighbors
+                for d in directions:
+                    neighbor_row, neighbor_col = row + d[0], col + d[1]
+                    if ROWS > neighbor_row >= 0 and COLS > neighbor_col >= 0:
+                        if grid[neighbor_row][neighbor_col] == 1:
+                            # this orange would be contaminated
+                            grid[neighbor_row][neighbor_col] = 2
+                            fresh_oranges -= 1
+                            # this orange would then contaminate other oranges
+                            queue.append((neighbor_row, neighbor_col))
+
+        # return elapsed minutes if no fresh orange left
+        return minutes_elapsed if fresh_oranges == 0 else -1
+
+
+
+
+
 #bfs solution using 2 deques
 #time complexity O(m * n)
 #space complexity O(m * n)
