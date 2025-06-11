3aebf661bb2aac21640d29f81dd5fd91fd5c66b2
Kyle Graham <17037205+KyleGraham@users.noreply.github.com>
2025-06-10 17:46:42 -0700

    another one

---
+++ salesforce/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,36 @@
+#You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+#You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+#Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+#brute force
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices)):
+            r = len(prices) - 1
+            while i < r:
+                profit = max(profit, prices[r] - prices[i])
+                r -= 1
+        return profit
+      
+      
+#two pointer (sliding window)
+#start 1 pointer at start and other pointer at start + 1
+#right pointer checks if the value is lower than the left pointer for a buy value
+# checks if the right pointer value is greater than the left pointer for a sell value
+# if it is, calculates the sell value
+# if not, buy price is lower than current, set the left pointer to the right pointer and increments the right pointer
+
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+      l, r = 0, 1
+      maxP = 0
+
+      while r < len(prices):
+          if prices[l] < prices[r]:
+              profit = prices[r] - prices[l]
+              maxP = max(maxP, profit)
+          else:
+              l = r
+          r += 1
+      return maxP
